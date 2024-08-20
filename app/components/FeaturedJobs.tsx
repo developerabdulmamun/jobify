1bb4eb31adba3b7a88f13f7c45a40d639dc3fce2
developerabdulmamun <abdulmamun.dev@gmail.com>
2024-08-20 21:29:23 +0600

    Featured Job section design complete with responsive

--- app/components/FeaturedJobs.tsx
+++ app/components/FeaturedJobs.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Heading from "./Heading";
+import JobData from "@/data";
+import Link from "next/link";
+import JobCard from "./JobCard";
 
 const FeaturedJobs = () => {
   return (
@@ -8,6 +11,22 @@ const FeaturedJobs = () => {
         mainHeading="Featured Jobs"
         subHeading="Know your worth and find the job that qualify your life"
       />
+
+      <div className="mt-12 w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12">
+        {JobData.map((job) => (
+          <Link key={job.id} href={`/job/jobdetails/${job.id}`}>
+            <JobCard job={job} />
+          </Link>
+        ))}
+      </div>
+
+      <Link href={"/job/alljobs"}>
+        <div className="text-center mt-[3rem]">
+          <button className="px-8 py-2 font-semibold hover:bg-blue-900 transition-all duration-300 bg-blue-700 rounded-lg text-white">
+            View All Jobs
+          </button>
+        </div>
+      </Link>
     </section>
   );
 };
